d089c902a28061fa57edeee80f668ef0bce95f95
pablogr2928@gmail.com <pablogr2928@gmail.com>
2024-10-16 11:15:10 -0400

    actualizacion por Pablo

--- database/migrations/2024_10_16_142159_create_inventarios_table.php
+++ database/migrations/2024_10_16_142159_create_inventarios_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('inventarios', function (Blueprint $table) {
             $table->id();
+            $table->integer('stock');
             $table->timestamps();
         });
     }
