4c1eb34b1ffa804b6f4a10d20078d28131a187e4
Rafael Oporto <cb.rafael.oporto.v@upds.net.bo>
2024-10-16 22:13:33 -0400

    modified:   reparar.bat

--- reparar.bat
+++ reparar.bat
@@ -1,4 +1,4 @@
-composer install
-cp .env.example .env
-php artisan key:generate
-php artisan migrate
+call composer install
+call copy .env.example .env
+call php artisan key:generate
+call php artisan migrate
